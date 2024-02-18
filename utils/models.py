b9c3e04aacc1b0068650db0b38e3f6addfc808a4
Alex1iv <ivanov.politehnik@gmail.com>
2024-02-18 16:17:59 +0600

    upd models architecture

---
+++ utils/models.py
@@ -0,0 +1,63 @@
+from keras.models import Model
+
+import tensorflow as tf
+# from keras.models import Sequential
+# from keras.layers import LSTM, Dense
+
+
+class ModelLSTM(Model):
+    """Класс создаёт модель LSTM, наследуя класс от tf.keras.
+    Параметры:
+    ----------
+    n_timesteps (_int_) - кол-во временных периодов
+    n_channels (_int_) - кол-во датчиков
+    output_units - конечный слой модели
+    lstm_units - размерность модели из конфига
+    """
+    def __init__(self, data):
+    
+        super().__init__()
+        # ------- параметры ------------
+        _, self.n_timesteps, self.n_channels = data.shape
+        
+        # -------- слои модели ----------------
+        self.input_channels = x = tf.keras.layers.Input(shape=(self.n_timesteps, self.n_channels))# #shape=(self.n_timesteps, self.n_channels)
+      
+        x = tf.keras.layers.LSTM(units=28, return_sequences=True)(x)
+        x = tf.keras.layers.LSTM(units=14, return_sequences=True, dropout=0.5)(x) #
+        #x = tf.keras.layers.LSTM(units=7)(x) #, dropout=0.5
+        #x = tf.keras.layers.Dense(1, activation='relu')(x)
+        #x = tf.keras.layers.BatchNormalization()(x)
+        #x = tf.keras.layers.Dropout(0.5)(x)
+
+        self.output_channels = tf.keras.layers.Dense(units=1, activation='relu')(x)
+        
+        print(f"input_shape = {(self.n_timesteps, self.n_channels)} | output_units = {self.output_channels}")
+
+    def build_model(self):
+        """Initiate the model
+        """
+        model = tf.keras.Model(
+            inputs=self.input_channels,
+            outputs=self.output_channels,
+            name="model_LSTM"
+        )        
+        model.summary()
+        
+        #--------------
+        # compile model
+        #--------------
+        optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
+        model.compile(loss = 'mse', metrics = ['mae', 'mse'], optimizer = optimizer)
+
+        return model
+    
+    # def compile_model(model):
+    #     """Compile the regression model
+    #     """
+        
+    #     optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
+    #     model.compile(loss = 'mse', metrics = ['mae', 'mse'], optimizer = optimizer)
+        
+    #     return model 
+    
